0655fc9e74fa9cf0c1a6a6f33b0a300286046e26
pevax1000 <paul.a.evangelou@gmail.com>
2025-02-22 14:35:01 +0000

    done

--- calculator2.js
+++ calculator2.js
@@ -93,6 +93,9 @@ function operate() {
     num2 = parseInt(tempnum.join(""))
     console.log(operator)
 
+    // if (operator === " " || num1 === result || num2 === 0) {
+    //     result = 0
+    // }
     if (operator === "+") {
     result = num1 + num2
     console.log(result)
@@ -110,10 +113,25 @@ function operate() {
         }
 
      else if (operator === "%") {
+            if (num2 !== 0) {
             result = num1 / num2
             console.log(result)
             display.textContent = result
             }
+            else {
+                display.textContent = "You no math so good?"
+            }
+            }
+
+    else {
+        if ((num1 == 0 & num2 == 0)) {
+            display.textContent = "0"
+            result = 0
+        }
+        else if (operator === " ") {
+            result = 0
+        }
+    }
 
     tempnum = []
     num1 = result
@@ -129,6 +147,74 @@ function clear() {
     tempnum = []
     currentoperator = " "
 }
+
+
+const three = document.querySelector("#three")
+three.addEventListener('click', (event) => {
+    tempnum.push(3)
+    display.textContent = tempnum.join("")
+    console.log(tempnum),
+    (event)
+})
+
+const four = document.querySelector("#four")
+four.addEventListener('click', (event) => {
+    tempnum.push(4)
+    display.textContent = tempnum.join("")
+    console.log(tempnum),
+    (event)
+})
+
+const five = document.querySelector("#five")
+five.addEventListener('click', (event) => {
+    tempnum.push(5)
+    display.textContent = tempnum.join("")
+    console.log(tempnum),
+    (event)
+})
+
+const six = document.querySelector("#six")
+six.addEventListener('click', (event) => {
+    tempnum.push(6)
+    display.textContent = tempnum.join("")
+    console.log(tempnum),
+    (event)
+})
+
+const seven = document.querySelector("#seven")
+seven.addEventListener('click', (event) => {
+    tempnum.push(7)
+    display.textContent = tempnum.join("")
+    console.log(tempnum),
+    (event)
+})
+
+const eight = document.querySelector("#eight")
+eight.addEventListener('click', (event) => {
+    tempnum.push(8)
+    display.textContent = tempnum.join("")
+    console.log(tempnum),
+    (event)
+})
+
+const nine = document.querySelector("#nine")
+nine.addEventListener('click', (event) => {
+    tempnum.push(9)
+    display.textContent = tempnum.join("")
+    console.log(tempnum),
+    (event)
+})
+
+const zero = document.querySelector("#zero")
+zero.addEventListener('click', (event) => {
+    tempnum.push(0)
+    display.textContent = tempnum.join("")
+    console.log(tempnum),
+    (event)
+})
+
+
+
 
 
 
